6243e9174d4cd97436fd58615175c13ac562b567
BraxtonGoode <tjbgoode@gmail.com>
2025-06-12 12:56:58 -0700

    changed croquet to basketball

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 router.use('/volleyball', require('./volleyball'));
 router.use('/soccer', require('./soccer'));
-// router.use('croquet', require('./croquet'));
+
+
+
+// router.use('basketball', require('./basketball'));
 // router.use('/user', require('./user'));
 
 module.exports = router;
