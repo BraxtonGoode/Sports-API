4e68a3cd25d0980fc199c61f53a5feaf5fb66a96
freedommk <131212585+freedombyu@users.noreply.github.com>
2025-06-16 15:03:17 +0200

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  role: { type: String, enum: ['User', 'Admin'], default: 'User' },
+});
+module.exports = mongoose.model('User', userSchema);
